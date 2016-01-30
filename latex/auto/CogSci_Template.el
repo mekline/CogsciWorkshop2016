ecf49a83f68193a446f04ac7d8e01b1f3f5659c5
jkhartshorne <jkhartshorne@gmail.com>
2016-01-30 14:31:08 -0500

    Wrote introduction

--- latex/auto/CogSci_Template.el
+++ latex/auto/CogSci_Template.el
@@ -4,12 +4,23 @@
    (LaTeX-add-bibitems
     "ChalnickBillman1988a"
     "Feigenbaum1963a"
+    "Levin2011"
+    "Bowerman1989"
+    "Gopnikinpress"
+    "Wittengerg2014"
+    "Ettinger"
+    "Battaglia2013"
+    "Ambridge2013"
     "Hill1983a"
+    "Henrik2015"
     "HOT2015"
+    "Clark2004"
     "HOSULS"
     "OhlssonLangley1985a"
     "Matlock2001"
     "Lewis1978a"
     "NewellSimon1972a"
-    "ShragerLangley1990a")))
+    "Pinker1989"
+    "Radvansky2014"
+    "Levin2005")))
 
