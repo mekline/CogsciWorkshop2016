93be427a8fe0b638c746e957d377c003eb59a3d7
jkhartshorne <jkhartshorne@gmail.com>
2016-01-30 10:22:32 -0500

    JKH: Changed some file names. Added author information.

---
+++ latex/auto/AcquisitionTheoriesEvents.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook
+ "AcquisitionTheoriesEvents"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "10pt" "letterpaper")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "cogsci"
+    "pslatex"
+    "apacite")
+   (LaTeX-add-labels
+    "sample-table"
+    "sample-figure")
+   (LaTeX-add-bibliographies
+    "CogSci_Template")))
+
